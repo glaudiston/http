c41b5fc9838653a9fc58b161386cdc6e394969e6
Glaudiston Gomes da Silva <glaudiston.silva@synchro.com.br>
2022-06-12 14:46:34 -0300

    show log with colors when using a tty

--- src/logger.c
+++ src/logger.c
@@ -3,7 +3,21 @@
 #include "logger.h"
 
 void logger_reportf(const char *level, const char *fmt, ...) {
-  fprintf(stderr, "\n[%s]\t", level);
+  if (isatty(fileno(stderr))){
+    int c=37;
+    if ( memcmp(level, "DEBUG", 5) == 0 ) {
+	    c = 36;
+    } else if ( memcmp(level, "INFO", 4) == 0 ) {
+	    c = 32;
+    } else if ( memcmp(level, "WARN", 4) == 0 ) {
+	    c = 33;
+    } else if ( memcmp(level, "ERROR", 4) == 0 ) {
+	    c = 31;
+    }
+    fprintf(stderr, "\n[\e[%im%s\e[0m]\t", c, level);
+  } else {
+    fprintf(stderr, "\n[%s]\t", level);
+  }
   va_list ap;
   va_start(ap, fmt);
   vfprintf(stderr, fmt, ap);
